7a387af99929c7ffd4f140e5534b473c5b639639
Temi-Tade <Temi-Tade@users.noreply.github.com>
2025-12-14 11:54:56 +0000

    deploy: c23cda22d5bbc5be6d03e068f7fac6db1683be06

---
+++ _app/immutable/chunks/BdFlcfQT.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_pe35o8?.base??"/TextMoji",e=globalThis.__sveltekit_pe35o8?.assets??s;export{e as a,s as b};
--- _app/immutable/chunks/CJeEIN2f.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_y1epir?.base??"/TextMoji",e=globalThis.__sveltekit_y1epir?.assets??s;export{e as a,s as b};
